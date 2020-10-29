204e11e6a26869dacdc89e835b61a76256fd7a5e
Salomé Pelaez <spsalome@gmail.com>
2020-10-29 16:58:31 -0500

    Optimización

--- Museo EdeA/Assets/Scripts/Display.cs
+++ Museo EdeA/Assets/Scripts/Display.cs
@@ -7,6 +7,7 @@ public class Display : MonoBehaviour
     public Manager manager;
 
     public int _id; 
+    public int displayID;
 
     public void Start()
     {
@@ -26,8 +27,45 @@ public class Display : MonoBehaviour
         manager.playerControl = false;
         manager.currentCamera = this.gameObject.transform.GetChild(0).gameObject;
         Debug.Log(manager.currentCamera);
+        //displayID = _id;
+        //Debug.Log("Display number " + displayID);
         //Debug.Log(currentCamera);
         //manager.Cameras[_id].enabled = true;
         //manager.Cameras[0].enabled = false;
     }
+
+    /*public void GetID()
+    {
+        Cameras c;
+
+        switch(c)
+        {
+            case c.Main:
+            Debug.Log("a");
+            break;
+
+            case c.WallDisplay:
+            Debug.Log("b");
+            break;
+
+            case c.SphereDisplay:
+            _id = 2;
+            manager.Cameras[2].gameObject.SetActive(true);
+            break;
+
+            case c.HexagonDisplay:
+            _id = 2;
+            manager.Cameras[2].gameObject.SetActive(true);
+            break;
+
+        }
+    }
+
+    public enum Cameras
+    {
+        Main,
+        WallDisplay,
+        SphereDisplay,
+        HexagonDisplay
+    }*/
 }
