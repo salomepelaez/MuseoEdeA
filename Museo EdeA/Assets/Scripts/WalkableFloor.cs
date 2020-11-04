eab01c25948be8b147a807d14d7c232ed4055363
Salomé Pelaez <spsalome@gmail.com>
2020-11-04 13:10:17 -0500

    Retroalimentación (lógica)

---
+++ Museo EdeA/Assets/Scripts/WalkableFloor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkableFloor : MonoBehaviour
+{
+    public GameObject particles;
+
+    void Start()
+    {
+        particles.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        particles.SetActive(true);
+    }
+
+    void OnMouseOver()
+    {
+        ParticlesOnPoint();
+    }
+
+    void OnMouseExit()
+    {
+        particles.SetActive(false);
+    }
+
+    void ParticlesOnPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100)) 
+        {    
+            particles.transform.position = hit.point;
+        }
+    }
+}
