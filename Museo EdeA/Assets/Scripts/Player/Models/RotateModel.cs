a97b38b2be52fa99ddf6998764738635922537ce
Salomé Pelaez <spsalome@gmail.com>
2020-11-05 14:44:26 -0500

    Rotar los modelos

---
+++ Museo EdeA/Assets/Scripts/Player/Models/RotateModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour
+{
+    float speed = 50f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
+        }
+        
+    }
+}
