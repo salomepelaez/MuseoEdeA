3d2ce233282d5e50eafc9e7162f13bfaed7df89d
Salomé Pelaez <spsalome@gmail.com>
2020-11-11 14:41:39 -0500

    Security copy

--- Museo EdeA/Assets/Scripts/Player/Models/RotateModel.cs
+++ Museo EdeA/Assets/Scripts/Player/Models/RotateModel.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class RotateModel : MonoBehaviour
 {
+    Manager manager;
+
     float speed = 50f;
+
+    void Start()
+    {
+        manager = Manager.Instance;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && manager.playerControl == false)
         {
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
         }
