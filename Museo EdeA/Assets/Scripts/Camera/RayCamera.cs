5cad18007f3793de19226803ccbce7fdfa34e6bf
Salomé Pelaez <spsalome@gmail.com>
2020-12-07 17:08:20 -0500

    Minijuego de túneles

---
+++ Museo EdeA/Assets/Scripts/Camera/RayCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCamera : MonoBehaviour
+{
+    Ray ray;    
+    RaycastHit hit;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = transform.GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            DeformMesh();
+        }
+    }
+
+    void DeformMesh()
+    {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            DeformPlane deform = hit.transform.GetComponent<DeformPlane>();
+
+            deform.DeformThisPlane(hit.point);
+        }
+    }
+}
