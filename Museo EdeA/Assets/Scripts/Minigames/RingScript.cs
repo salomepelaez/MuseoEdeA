f2653ea7cae1f3b6a43ba53cc6ba04dea2649bc6
Salomé Pelaez <spsalome@gmail.com>
2020-12-14 21:03:40 -0500

    Funciona la excavación

---
+++ Museo EdeA/Assets/Scripts/Minigames/RingScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "RingCollider")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
