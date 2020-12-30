d59df09d9e515f927848878654c2395f9cdf091a
Salomé Pelaez <spsalome@gmail.com>
2020-12-30 13:38:22 -0500

    Plataformas

---
+++ Museo EdeA/Assets/FollowCricket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCricket : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.3f;
+    
+    void LateUpdate()
+    {
+        if(target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+    }
+}
